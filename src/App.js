0f400ecb96b5af4dd2dba17e86d9b53c91213218
Aa-thomas <aarthomas01@gmail.com>
2023-02-23 20:01:23 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import UserContext from './utilities/UserContext';
 import { useEffect } from 'react';
 import axios from 'axios';
+import Courses from '@components/Courses';
+import { Route } from 'react-router-dom';
+import CreateCourse from '@components/CreateCourse';
 
 function App() {
 	useEffect(() => {
@@ -11,7 +12,19 @@ function App() {
 		);
 	});
 
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Courses />} />
+				<Route path="/courses/create" element={<CreateCourse />} />
+				<Route path="/courses/:id/update" element={<UpdateCourse />} />
+				<Route path="/courses/:id" element={<CourseDetail />} />
+				<Route path="/signin" element={<UserSignIn />} />
+				<Route path="/signup" element={<UserSignUp />} />
+				<Route path="/signout" element={<UserSignOut />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
