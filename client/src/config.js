b5b1e1b41b0898d19bb3cfe017a07448a1806709
Aa-thomas <aarthomas01@gmail.com>
2023-03-15 19:33:24 -0400

    deploy 2

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,5 @@
 const config = {
-	apiBaseUrl: 'http://localhost:5000/api',
+	apiBaseUrl: 'https://remotelearners-api.up.railway.app/',
 };
-// https://cors-anywhere.herokuapp.com/
-// https://remotelearners-api.up.railway.app/
+
 export default config;
